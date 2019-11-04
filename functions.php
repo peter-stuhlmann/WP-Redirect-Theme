a2b982f9338556803eb8d31fa24184f32db62d57
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-11-04 19:18:30 +0100

    Add german translation for settings page

--- functions.php
+++ functions.php
@@ -9,8 +9,8 @@ add_action( 'admin_enqueue_scripts', 'wpr_admin_styles' );
 function wpr_theme_settings() {
 	add_submenu_page(
 		'options-general.php',                 // parent slug
-		'WP Redirect (Headless CMS) Settings', // page title
-		'Theme settings',                      // menu title
+		__('WP Redirect (Headless CMS) Settings', 'wp-redirect'), // page title
+		__('Theme settings', 'wp-redirect'),   // menu title
 		'manage_options',                      // capability
 		'wp-redirect-theme-settings',          // menu slug
 		'wpr_theme_settings_html'              // callback function to be called when rendering the page
@@ -36,7 +36,7 @@ function wpr_settings_init() {
     
 	add_settings_field(
 		'redirect-url',                        // id
-		'Redirect URL',                        // title
+		__('Redirect URL', 'wp-redirect'),   // title
 		'wpr_url_inputfield',                  // callback function
 		'wp-redirect-theme-settings',          // page
         'wpr-settings-section',                // section
@@ -55,19 +55,26 @@ function wpr_url_inputfield() {
 function wpr_theme_settings_html() {
     ?>
     <div class="wrap">
-        <h1>WP Redirect (Headless CMS) Settings</h1>
-        <p>Please enter the URL to which the visitor should be redirected.</p>
+        <h1><?php _e('WP Redirect (Headless CMS) Settings', 'wp-redirect');  ?></h1>
+        <p><?php _e('Please enter the URL to which the visitor should be redirected.', 'wp-redirect');  ?></p>
         <form method="POST" action="options.php">
             <?php settings_fields('wp-redirect-theme-settings');?>
             <?php do_settings_sections('wp-redirect-theme-settings')?>
             <?php submit_button(
-                null,                               // text
-                'primary',                          // type
-                'submit',                           // name
-                true,                               // wrap
-                null                                // other attributes
+                null,                          // text
+                'primary',                     // type
+                'submit',                      // name
+                true,                          // wrap
+                null                           // other attributes
             )?>
         </form>
     </div>
 <?php
 }
+
+ 
+// Translation
+function wpr_theme_setup(){
+    load_theme_textdomain('wp-redirect',   get_template_directory() . '/assets/languages');
+}
+add_action('after_setup_theme', 'wpr_theme_setup');
